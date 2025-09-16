ae373cc2f989fb744fcfcd5950ec04209fb61db9
ArthemisDesign <93138273+ArthemisDesign@users.noreply.github.com>
2025-09-16 20:44:44 +0300

    fix: resolve merge conflicts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,16 +8,13 @@ const config: Config = {
     extend: {
       fontFamily: {
         sans: ['Aeonik Pro', 'system-ui', 'sans-serif'],
-<<<<<<< HEAD
         'aeonik-bold': ['AeonikProBold', 'sans-serif'],
         'aeonik-regular': ['Aeonik Pro Regular', 'sans-serif'],
+        'ogg': ['Ogg', 'serif'],
       },
       letterSpacing: {
         'extra-wide': '0.1em',
       }
-=======
-      },
->>>>>>> 17f7ef69d1262517ee98cee7187a17bbce532328
     },
   },
   plugins: [],
